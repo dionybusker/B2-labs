e7b0e845cf8b7ca44ee2414709a93045745fe7ad
dionybusker <99057959@mydavinci.nl>
2019-11-19 14:31:18 +0100

    w.i.p.

--- B2W11/js/opdracht_2.js
+++ B2W11/js/opdracht_2.js
@@ -4,3 +4,9 @@ var newName = prompt("Hoeveel namen wilt u in de array stoppen? (Minimaal 3)");
 
 
 // names.push()
+
+
+while (names == newName) {
+    names.push(newName);
+    document.getElementById("namen").innerHTML = names;
+}
--- B2W11/js/opdracht_3.js
+++ B2W11/js/opdracht_3.js
@@ -1,18 +1,22 @@
 var arrayEen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var arrayTwee = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
-function add() {
-    // optellen
+// arrayEen + " + " + arrayTwee + " = " + arrayEen
+
+function optellen(arrayEen, arrayTwee) {
+
+    // var sum = document.getElementById("optellen").innerHTML = `${arrayEen[0]} + ${arrayTwee[0]} = `;
+
+    for (var i = 0; i < arrayEen.length; i++) {
+        sum = `${arrayEen[i]} + ${arrayTwee[i]} = `;
+    }
+
+
+    for (var i = 0; i < arrayEen.length; i++) {
+        arrayEen[i] += arrayTwee[i];
+    }
+
+    document.getElementById("optellen").innerHTML = sum + arrayEen;
 }
 
-function subtraction() {
-    // aftrekken
-}
-
-function multiply() {
-    // vermenigvuldigen
-}
-
-function divide() {
-    // delen
-}
+optellen(arrayEen, arrayTwee);

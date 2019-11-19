df333d299cb4bf5886bda98c27b9612b99d58dbc
dionybusker <99057959@mydavinci.nl>
2019-11-19 09:01:45 +0100

    opdr 3 toegevoegd
    w.i.p.

---
+++ B2W11/js/opdracht_3.js
@@ -0,0 +1,18 @@
+var arrayEen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arrayTwee = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+function add() {
+    // optellen
+}
+
+function subtraction() {
+    // aftrekken
+}
+
+function multiply() {
+    // vermenigvuldigen
+}
+
+function divide() {
+    // delen
+}

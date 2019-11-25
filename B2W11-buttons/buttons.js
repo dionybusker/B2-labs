5871f6e7617cab53a029b03a483a070683f1c852
dionybusker <99057959@mydavinci.nl>
2019-11-25 12:42:08 +0100

    buttons/img veranderen bij onclick
    buttons veranderen nu als je erop drukt, dan wordt de waarde 1, de images veranderen dan ook mee

--- B2W11-buttons/buttons.js
+++ B2W11-buttons/buttons.js
@@ -2,6 +2,22 @@
 
 // img.src = "images/1.jpg";
 
-function changeImage() {
-    document.getElementById("image").innerHTML = url("images/1.jpg");
+// var button1 = document.getElementById("button1").innerHTML;
+// var button2 = document.getElementById("button2").innerHTML;
+// var button3 = document.getElementById("button3").innerHTML;
+
+function changeImage(number) {
+    if (number == 1) {
+        document.getElementById("image").src = "images/bg1.jpg";
+        document.getElementById("small-image").src = "images/1.jpg";
+        document.getElementById("num1").innerHTML = 0 + 1;
+    } else if (number == 2) {
+        document.getElementById("image").src = "images/bg2.jpg";
+        document.getElementById("small-image").src = "images/2.jpg";
+        document.getElementById("num2").innerHTML = 0 + 1;
+    } else if (number == 3) {
+        document.getElementById("image").src = "images/bg3.jpg";
+        document.getElementById("small-image").src = "images/3.jpg";
+        document.getElementById("num3").innerHTML = 0 + 1;
+    }
 }

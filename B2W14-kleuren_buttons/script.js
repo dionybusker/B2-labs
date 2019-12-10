6d7e6d6ef923a42ca6e4cd62e7454e257c6df0f0
dionybusker <99057959@mydavinci.nl>
2019-12-10 14:33:50 +0100

    lab 2, mee bezig

--- B2W14-kleuren_buttons/script.js
+++ B2W14-kleuren_buttons/script.js
@@ -3,6 +3,10 @@ container.style.backgroundColor = "grey";
 container.style.width = "1200px";
 container.style.margin = "0 auto";
 
-var button = document.createElement("button");
-container.appendChild(button);
-button.innerText = "button";
+
+
+for (i = 1; i <= 30; i++) {
+    var button = document.createElement("button");
+    container.appendChild(button);
+    button.innerText = i;
+}

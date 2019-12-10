6024f83006792e28d036ed19dc056113c5950d7a
dionybusker <99057959@mydavinci.nl>
2019-12-10 12:18:16 +0100

    container en button gemaakt

---
+++ B2W14-kleuren_buttons/script.js
@@ -0,0 +1,8 @@
+var container = document.getElementById("container");
+container.style.backgroundColor = "grey";
+container.style.width = "1200px";
+container.style.margin = "0 auto";
+
+var button = document.createElement("button");
+container.appendChild(button);
+button.innerText = "button";
